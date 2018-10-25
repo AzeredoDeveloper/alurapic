21dc2a382e66e6d68ce452d2f043872299f68296
Daniel Azeredo <djmetal82@yahoo.com.br>
2018-10-25 14:41:01 -0300

    Acesso do headers de resposta

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { observable } from 'rxjs';
 
 const URL_API = 'http://localhost:3000'
 
@@ -13,7 +15,12 @@ export class AuthService {
 
   autenticate(userName: string, password: string) {
     
-    return this.http.post(URL_API + '/user/login', {userName, password});
+    return this.http.post(URL_API + '/user/login', {userName, password}, {observe: 'response'})
+    .pipe(tap( res => {
+      const authToken = res.headers.get('x-access-token');
+      console.log(authToken);
+      
+    }));
 
   }
 }
