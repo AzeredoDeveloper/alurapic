75cfd50a98b43d337dd759583909f758a04405ea
Daniel Azeredo <djmetal82@yahoo.com.br>
2018-10-25 11:07:52 -0300

    ViewChild: obtendo referÊncias do template

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { AuthService } from 'src/app/core/auth.service';
 import { Router } from '@angular/router';
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>
 
   constructor(private formBuilder: FormBuilder, private service: AuthService, private router: Router) { }
 
@@ -25,12 +26,13 @@ export class SigninComponent implements OnInit {
     const password = this.loginForm.get('password').value;
 
     this.service.autenticate(userName, password).subscribe(
-      retorno => {
+      () => {
         this.router.navigate(['user', userName]);
 
       },
       error => {
         this.loginForm.reset();
+        this.userNameInput.nativeElement.focus();
        alert('Usuário e/ou senha inválido(s).');
       }
     );
