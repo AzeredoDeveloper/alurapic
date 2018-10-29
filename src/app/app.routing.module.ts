720d17576cd3932464ed3a33a92adc20e02507f6
Daniel Azeredo <djmetal82@yahoo.com.br>
2018-10-29 09:36:57 -0300

    O componente de registro

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SigninComponent } from './home/signin/signin.component';
 import { AuthGuardService } from './core/auth/auth-guard.service';
+import { SignupComponent } from './home/signup/signup.component';
 
 const routes: Routes = [
     { 
@@ -24,6 +25,10 @@ const routes: Routes = [
     { 
         path: 'p/add', 
         component: PhotoFormComponent 
+    },
+    { 
+        path: 'signup', 
+        component: SignupComponent 
     },
     { 
         path: '**', 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
 import { SignoutComponent } from './signout/signout.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     VMessageModule,
     RouterModule
   ],
   declarations: [
     SigninComponent, 
-    SignoutComponent
+    SignoutComponent,
+    SignupComponent
   ]
 })
 export class HomeModule { }
